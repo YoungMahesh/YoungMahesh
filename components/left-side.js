9a4ff55b5ed6a7e1438c1903d6a033c116c0b894
YoungMahesh <maheshghamand@gmail.com>
2020-06-14 13:06:16 +0530

    left-section improved

--- components/left-side.js
+++ components/left-side.js
@@ -1,23 +1,23 @@
 export default function LeftSide () {
    return(
       <div className='left'>
-         <div className='left-inner'>
-            <div className='info'>
-               <h2 className='name'>Hey, I am Mahesh</h2>
-               <p className='subtext'>Web developer, based in Beed, Maharashtra.</p>
-               <div className='social-links'>
-                  <a className='icon1' href='https://twitter.com/MGhamand' target='_blank'>
-                  <i className="fab fa-twitter fa-2x" style={{color: 'black'}}></i>
-                  </a>
-                  <a className='icon1' href='https://github.com/YoungMahesh' target='_blank'>
-                  <i className="fab fa-github fa-2x" style={{color: 'black'}}></i>
-                  </a>
-                  <a className='icon1' href='https://www.linkedin.com/in/mahesh-ghamand-114a7b191/' target='_blank'>
-                  <i className="fab fa-linkedin-in fa-2x" style={{color: 'black'}}></i>
-                  </a>
-               </div>
+         <div className='info'>
+            <h2 className='name'>Hey, I am Mahesh</h2>
+            <p className='subtext'>Web developer, based in Beed, Maharashtra.</p>
+            <div className='social-links'>
+               <a className='icon1' href='https://twitter.com/MGhamand' target='_blank'>
+               <i className="fab fa-twitter fa-lg" style={{color: 'white'}}></i>
+               </a>
+               <a className='icon1' href='https://github.com/YoungMahesh' target='_blank'>
+               <i className="fab fa-github fa-lg" style={{color: 'white'}}></i>
+               </a>
+               <a className='icon1' href='https://www.linkedin.com/in/mahesh-ghamand-114a7b191/' target='_blank'>
+               <i className="fab fa-linkedin-in fa-lg" style={{color: 'white'}}></i>
+               </a>
             </div>
          </div>
+
+
       </div>
    )
 }
